709fb1c0c35e4f375ec46963ea7fa40d560d9b7d
N3002 User <N3002user@N3002.uc-bcf.edu.ph>
2024-02-21 12:17:44 +0800

    second commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
-}
+public function index(Request $request)
+{
+$theme = $request->input('theme', 'gadgets');
+
+$products = [
+    'gadgets' => [
+    'iPhone 12',
+    'Samsung Galaxy S21',
+    'Google Pixel 5',
+    ],
+    'anime' => [
+    'Attack on Titan',
+    'One Punch Man',
+    'Naruto',
+    ],
+    ];
+    
+    return view('products.index', [
+    'theme' => $theme,
+    'products' => $products[$theme],
+    ]);
+    }
+    }
